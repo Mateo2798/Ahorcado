572b088a2cddd04f7813ab10fce5ae51e30400ae
MateoVasquez <jos3guzman90@gmail.com>
2022-09-18 21:14:00 -0600

    Agregando footer y configurando que el año sea el actual con js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 var startGame = document.getElementById("startGame");
 var addWord = document.getElementById("addWord");
+var year = document.getElementById("year");
+
+function yearActual(){
+    copyright = new Date();
+    yearUpdate = copyright.getFullYear();
+    
+    return yearUpdate;
+}
+year.innerText = yearActual();
