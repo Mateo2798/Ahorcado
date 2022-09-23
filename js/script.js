69b454daa33589b68e25545244d20d77f0330501
MateoVasquez <jos3guzman90@gmail.com>
2022-09-22 21:36:45 -0600

    creando la funcion para escribir las letras correctas sobre las lineas

--- js/script.js
+++ js/script.js
@@ -4,35 +4,62 @@ var saveStart = document.getElementById("saveStart");
 var cancel = document.getElementById("cancel");
 
 /*Contenedores */
-var botonesPrincipal =  document.getElementsByClassName("contenedor-botonesPrincipal");
+var botonesPrincipal = document.getElementsByClassName("contenedor-botonesPrincipal");
 var contenedorGame = document.getElementsByClassName("contenedor-Game");
 var scriptContent = document.getElementById("contenedor-guiones");
+var guiones = document.getElementsByClassName("line");
 
 var year = document.getElementById("year");
-var secretWords = ["pajaro","avion","manzana","elefante"];
 
-function yearActual(){
+/*Variables del juego */
+var palabraSecreta = "";
+var secretWords = ["pajaro", "avion", "manzana", "elefante"];
+
+function yearActual() {
     copyright = new Date();
     yearUpdate = copyright.getFullYear();
-    
+
     return yearUpdate;
 }
 
-function createSecretWord(){
-    var numbRandom = Math.round(Math.random() * (secretWords.length -1));
-    console.log(secretWords[numbRandom] + " " + numbRandom)
-    
-   drawLines(secretWords[numbRandom].length);
+function newGame() {
+    createSecretWord();
+    drawLines(palabraSecreta.length);
 
-   window.onkeydown = keyDown;
-   botonesPrincipal[0].style.display = "none";
-   contenedorGame[0].style.display = "flex";
+    document.onkeydown = (e) => {
+        let letra = e.key;
+        let code = e.keyCode;
+        
+        if(keyDown(code) && palabraSecreta.includes(letra)){
+            for(let i = 0; i < palabraSecreta.length; i++){
+                /*Draw letter on the line*/
+                if(palabraSecreta[i] === letra){
+                    guiones[i].innerHTML = letra;
+                }
+            }
+        }else{
+            
+        }
+    }
+
 }
 
-function drawLines(number){
+function createSecretWord() {
+    var numbRandom = Math.round(Math.random() * (secretWords.length - 1));
+    console.log(secretWords[numbRandom] + " " + numbRandom);
+
+    /*Palabra elegida aleatoriamente*/
+    palabraSecreta = secretWords[numbRandom];
+
+    /*Elementos de la pantalla */
+    botonesPrincipal[0].style.display = "none";
+    contenedorGame[0].style.display = "flex";
+}
+
+function drawLines(number) {
     scriptContent.innerHTML = "";
 
-    for(var i = 0; i < number; i++){
+    for (var i = 0; i < number; i++) {
         var divs = document.createElement("div");
         divs.id = i;
         divs.className = "line"
@@ -40,15 +67,19 @@ function drawLines(number){
     }
 }
 
-function keyDown(e){
-    var code = e.keyCode;
-    var letra = e.key;
-    if((code >= 65 && code <= 90) | code == 192){
-        return letra;
+function keyDown(code) {
+    if ((code >= 65 && code <= 90) || code == 192) {
+        return true;
+    } else {
+        return false;
     }
-    return 0;
 }
 
+function drawWords(letras) {
 
-startGame.addEventListener("click",createSecretWord);
+
+
+}
+
+startGame.addEventListener("click", newGame);
 year.innerText = yearActual();
